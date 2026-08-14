2b559e0fb4afec7107e76f1351a40caa38a9b23a
Sreedhar Naik <sreedharnaiksugali45@gmail.com>
2026-08-14 15:28:43 +0530

    Implement iterative factorial function

---
+++ Factorial_iterative.py
@@ -0,0 +1,10 @@
+def factorial_iterative(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact = fact * i
+    return fact
+n = int(input("Enter a number: "))
+if n < 0:
+    print("Factorial is not defined for negative numbers")
+else:
+    print("Factorial =", factorial_iterative(n))
